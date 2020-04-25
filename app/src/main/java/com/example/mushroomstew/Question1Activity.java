98300ae7dbc1e15eac76924c73ddeac37c31e81d
mbs700 <mbs7@illinois.edu>
2020-04-25 18:18:23 -0500

    Saving work before I start deleting stuff to try and deal with an error

--- app/src/main/java/com/example/mushroomstew/Question1Activity.java
+++ app/src/main/java/com/example/mushroomstew/Question1Activity.java
@@ -6,14 +6,41 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class Question1Activity extends AppCompatActivity {
+    //public static final String EXTRA_NUMBER = "com.example.application.mushroomstew.EXTRA_NUMBER";
+
+
+    private TextView sliderValue;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question1);
+
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        sliderValue = findViewById(R.id.sliderValue);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                sliderValue.setText(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
 
         Button continueButton;
         continueButton = findViewById(R.id.continueButton);
@@ -26,7 +53,11 @@ public class Question1Activity extends AppCompatActivity {
     }
 
     private void openQ2Activity() {
+        //TextView previousSum = findViewById(R.id.textView);
+       // int sum = Integer.parseInt(previousSum.getText().toString());
+
         Intent intent = new Intent(this, Question2Activity.class);
+        //intent.putExtra(EXTRA_NUMBER, sum);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/mushroomstew/Question2Activity.java
+++ app/src/main/java/com/example/mushroomstew/Question2Activity.java
@@ -6,15 +6,43 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+//import android.widget.TextView;
 
 public class Question2Activity extends AppCompatActivity {
 
+    //private TextView previousSum = findViewById(R.id.previousSum);
 
+    private TextView sliderValue = findViewById(R.id.sliderValue);
+    private SeekBar seekBar = findViewById(R.id.seekBar);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question2);
+
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                sliderValue.setText(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        //Intent intent = getIntent();
+        //int sum = intent.getIntExtra(Question1Activity.EXTRA_NUMBER, 0);
+        //previousSum.setText(sum);
 
         Button continueButton;
         continueButton = findViewById(R.id.continueButton);
